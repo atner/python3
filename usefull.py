cb478f3d495491cb3480fc2c801b61de812d11fd
atner <atnerus88@gmail.com>
2017-12-20 17:50:30 +0300

    Create usefull.py

---
+++ usefull.py
@@ -0,0 +1,8 @@
+#цикл в списке, который также отслеживает индекс текущего элемента. 
+students = ('James', 'Andrew', 'Mark')
+for i, student in enumerate(students):
+    print i, student
+# вывод:
+# 0 James
+# 1 Andrew
+# 2 Mark 
